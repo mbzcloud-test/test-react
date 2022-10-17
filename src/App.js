fb00891bb68a0cb29bdbc84bb568bff2755f2bcd
TrevorNathan <trevoratwijukire@gmail.com>
2022-10-17 10:05:49 +0300

    UPDATE: v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import AppPackage from "@mbzcloud-test/test-package/src/App";
+
+// Use the exported function from package
+//console.log(sum(1, 2));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <AppPackage />
     </div>
   );
 }
